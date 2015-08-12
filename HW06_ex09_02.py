a1ed56987fdac8820c551a1638c6649f2f6f476a
Laura Desmond-Black <ladesmondblack@gmail.com>
2015-08-12 09:17:24 -0700

    One homework for day 6

--- HW06_ex09_02.py
+++ HW06_ex09_02.py
@@ -14,10 +14,45 @@
 
 # Body
 
+def has_no_e(word):
+	'''
+	Test if a word contains the letter "e"
+	'''
+	if 'e' in word:
+		return False
+	else:
+		return True
+
+
+
+def read_text_file():
+	'''
+	In a given set of words find all words that do not contain 'e'.
+	Calculate that percentage of words as a part of the given set.
+	'''
+	count_no_e = 0
+	total_words = 0
+	fin = open("words.txt")
+	for line in fin:
+		total_words += 1
+		word = line.strip()
+		if has_no_e(word):
+			print(word + "\n")
+			count_no_e += 1
+	fin.close
+	percentage = (float(count_no_e)/total_words) * 100
+	print("The percentage of words with no 'e' is: " + str(percentage))
+
+		
+
+
+
 
 ##############################################################################
 def main():
-    pass  # Call your function(s) here.
+    read_text_file()
 
 if __name__ == '__main__':
     main()
+
+
